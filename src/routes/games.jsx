ed0b6e5992e1f215151654aee66005d828a32f1c
markpandan <72681401+markpandan@users.noreply.github.com>
2025-07-17 22:24:08 +0800

    Add image utils for positionings

--- src/routes/games.jsx
+++ src/routes/games.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react";
 import styles from "./games.module.css";
 import BoundingBox from "../components/BoundingBox";
+import {
+  getContainedSize,
+  getContainedX,
+  getNaturalPosition,
+} from "../utils/imageUtils";
 
 const Games = () => {
   const [pos, setPos] = useState();
+
   const handleClick = (e) => {
-    setPos([e.nativeEvent.offsetX, e.nativeEvent.offsetY]);
+    const dimensions = getContainedSize(e.target);
+
+    const containedX = getContainedX(
+      e.nativeEvent.offsetX,
+      dimensions.containedWidth,
+      e.target.width
+    );
+    const containedY = e.nativeEvent.offsetY;
+
+    if (containedX) {
+      console.log(
+        getNaturalPosition([containedX, containedY], dimensions.scale)
+      );
+    }
+  };
+
+  const handleLoad = (e) => {
+    const naturalWidth = e.target.naturalWidth;
+    const naturalHeight = e.target.naturalHeight;
+
+    console.log({ naturalWidth, naturalHeight });
   };
 
   return (
     <>
-      <div
-        className={`flex-container ${styles.gameContainer}`}
-        onClick={(e) => handleClick(e)}
-      >
+      <div className={`flex-container ${styles.gameContainer}`}>
         <img
           className={styles.gameImage}
           src="/src/assets/genshin-impact.jpeg"
+          onClick={handleClick}
+          onLoad={handleLoad}
         />
         {pos && <BoundingBox pos={pos} />}
         <div className={styles.gameObjectives}>
---
+++ src/utils/imageUtils.js
@@ -0,0 +1,31 @@
+export const getContainedSize = (img) => {
+  let { width, height, naturalWidth, naturalHeight } = img;
+  let iAR = naturalWidth / naturalHeight;
+  let ctAR = width / height;
+  let [containedWidth, containedHeight] =
+    iAR >= ctAR ? [width, width / iAR] : [height * iAR, height];
+  let scale = containedWidth / naturalWidth;
+  return { containedWidth, containedHeight, scale };
+};
+
+export const getContainedX = (x, imgWidth, containerWidth) => {
+  const barWidth = Math.round((containerWidth - imgWidth) / 2);
+
+  const leftBarPos = barWidth;
+  const rightBarPos = barWidth + imgWidth;
+
+  if (x < leftBarPos || x > rightBarPos) {
+    return undefined;
+  }
+
+  return x - barWidth;
+};
+
+export const getNaturalPosition = (containedPos, scale) => {
+  const [containedX, containedY] = containedPos;
+
+  const naturalX = containedX / scale;
+  const naturalY = containedY / scale;
+
+  return { naturalX, naturalY };
+};
